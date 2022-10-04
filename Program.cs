c72ed0db273b807a3dc5f6357d96443bc0a96e77
Ilya_Moroz <moroz-il@yandex.ru>
2022-10-04 13:08:07 +0300

    начало метода

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+string[] array = new string[5] {"15555", "25555", "333","433", "5444"};
+string[] finalarray = new string[array.Length]
+
+void ArrayWithIf(string[] array, string[] finalarray)
+{   
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+    }
+}
