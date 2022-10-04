573c8b405da3215a8cffb331cc9deb809edc00e4
Ilya_Moroz <moroz-il@yandex.ru>
2022-10-04 13:32:50 +0300

    finaltest

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] array = new string[5] {"15555", "25555", "333", "433", "5444"};
+﻿string[] array = new string[7] {"1555", "25555", "333", "433", "5444", "final", "alo"};
 string[] finalarray = new string[array.Length];
 
 void ArrayWithIf(string[] array, string[] finalarray)
