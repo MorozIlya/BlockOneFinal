98a332837adc3b3e5e29780a5f2167bde860df74
Ilya_Moroz <moroz-il@yandex.ru>
2022-10-27 10:00:33 +0300

    ispravleniya

--- Program.cs
+++ Program.cs
@@ -1,23 +1,21 @@
-﻿string[] array = new string[7] {"1555", "25555", "333", "433", "5444", "final", "alo"};
+﻿string[] array = new string[7] { "1555", "25555", "333", "433", "5444", "final", "alo" };
 string[] finalarray = new string[array.Length];
 
 void ArrayWithIf(string[] array, string[] finalarray)
-{   
+{
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-    if(array[i].Length <= 3)
+        if (array[i].Length <= 3)
         {
-        finalarray[count] = array[i];
-        count++;
+            finalarray[count] = array[i];
+            count++;
         }
     }
 }
 void PrintArray(string[] array)
 {
-    for ( int i = 0;
-    i < array.Length;
-    i++ )
+    for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"{array[i]} ");
     }
